c85ec19876a6c5373949316b6ca9df42c34e0549
21cygnus12 <zackgwaterman@gmail.com>
2025-07-02 15:08:43 -0700

    first video done

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,12 @@
-require("config.lazy")
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+
+vim.opt.scrolloff = 999
